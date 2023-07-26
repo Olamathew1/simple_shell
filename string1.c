4661d0f93dd9793c172bc6c276db095ad0805f85
Dennis William <dennisanietimfonwilliam005@gmail.com>
2023-07-26 11:43:40 +0100

    group project

---
+++ string1.c
@@ -0,0 +1,91 @@
+#include "shell.h"
+
+/**
+ * _strcpy -  this will copies  string
+ * @dest: destination
+ * @src: the source
+ *
+ * Description: this will copies  string
+ * Return: pointer to destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
+/**
+ * _strdup - this to duplicates a string
+ * @str: the string to duplicate
+ *
+ * Description: this to duplicates a string
+ * Return: pointer to the duplicated string
+ */
+char *_strdup(const char *str)
+{
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}
+
+/**
+ * _puts - prints an input string
+ * @str: the string to be printed
+ *
+ * Description: prints an input string
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _putchar - this will write the character c to stdout
+ * @c: The character to print
+ *
+ * Description: this will write the character c to stdout
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is well set
+ */
+int _putchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
